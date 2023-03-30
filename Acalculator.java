d6f22bcc6206ec374850ea495af48398c86b533e
vaibhavdantakale1 <127470586+vaibhavdantakale1@users.noreply.github.com>
2023-03-30 11:23:09 +0530

    Add files via upload

---
+++ Acalculator.java
@@ -0,0 +1,9 @@
+package com.cognizant.shapes;
+
+public class Acalculator {
+	public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(5, 15);
+        rectangle.calculateArea();
+    }
+
+}
